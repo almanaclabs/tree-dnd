14cf938f6e6c45fa0f0fa6b1b63f903fb0c789fd
Danilo Resende <danilo.resende@almanac.io>
2022-01-17 16:57:46 -0300

    Remove leftover *.js files

--- src/utils/react.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.sameProps = void 0;
-exports.sameProps = function (oldProps, newProps, props) {
-    return props.find(function (p) { return oldProps[p] !== newProps[p]; }) === undefined;
-};
